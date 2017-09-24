c6b22d2a8fa6c54c650d3ced3f128edf9bb241cc
Hexter Weng <hexter.ch@gmail.com>
2017-09-24 13:34:34 +0800

    Basic function

--- pratice_1/main.go
+++ pratice_1/main.go
@@ -6,8 +6,15 @@ func add(x, y int) int {
 	return x + y
 }
 
-f
+// func name(params) (return result type) {
+// code
+// }
+func swap(x, y string) (string, string) {
+	return y, x
+}
 
 func main() {
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 	fmt.Println(add(42, 13))
 }
