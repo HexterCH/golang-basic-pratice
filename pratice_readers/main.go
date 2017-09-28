63eb48fbd4f65ac71b68e6cb6a78c3d25b81a331
Hexter Weng <hexter.ch@gmail.com>
2017-09-29 00:28:57 +0800

    rotreader exercise

--- pratice_readers/main.go
+++ pratice_readers/main.go
@@ -4,10 +4,43 @@ import (
 	"strings"
 	"fmt"
 	"io"
+	"golang.org/x/tour/reader"
+	"os"
 )
 
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (rot *rot13Reader) Read(p []byte) (n int, err error) {
+	n,err = rot.r.Read(p)
+	for i := 0; i < len(p); i++ {
+		if (p[i] >= 'A' && p[i] < 'N') || (p[i] >='a' && p[i] < 'n') {
+			p[i] += 13
+		} else if (p[i] > 'M' && p[i] <= 'Z') || (p[i] > 'm' && p[i] <= 'z'){
+			p[i] -= 13
+		}
+	}
+	return
+}
+
+type MyReader struct {}
+
+func (r MyReader ) Read(s []byte) (int, error) {
+	for i := range s {
+		s[i] = 'A'
+	}
+	return len(s), nil
+}
+
 func main() {
+	reader.Validate(MyReader{})
+
 	r := strings.NewReader("Hello, Reader!")
+
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	rotReader := rot13Reader{s}
+	io.Copy(os.Stdout, &rotReader)
 
 	b := make([]byte, 8)
 	for {
