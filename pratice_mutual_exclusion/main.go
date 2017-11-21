a94b951f802a3055a2258e4c763182495418af1f
Hexter Weng <hexter.ch@gmail.com>
2017-11-21 17:22:58 +0800

    Add sync pratice

---
+++ pratice_mutual_exclusion/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "sync"
+
+// SafeCounter is safe to use concurrently.
+type SafeCounter struct {
+	v map[string]int
+	mux sync.Mutex
+}
+
+// Inc increments the counter for the given key.
+func (c *SafeCounter) Inc(key string) {
+	c.mux.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	c.v[key]++
+	c.mux.Unlock()
+}
+
+// Value returns the current value of the counter for the given key.
